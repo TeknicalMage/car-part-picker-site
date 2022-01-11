e63184d0932ffb1a512785b7852cf6d2a7288c6c
Teknical Mage <arcazjordan@gmail.com>
2022-01-11 00:53:45 -0500

    Hold up

---
+++ carpartpicker/src/Nav_G2.js
@@ -0,0 +1,38 @@
+//import { Link } from 'react-router-dom';
+//import './Nav.css';
+//import React, { useState } from "react"
+//import { Card, Button, Alert } from "react-bootstrap"
+//  import { useAuth } from "./contexts/AuthContext"
+//import { useHistory } from "react-router-dom"
+
+//export default function Nav() {
+//const { currentUser } = useAuth()
+
+
+//return<nav>
+//        <ul className = "grid-container">
+//          <Link to='/Homepage' className="grid-item"  style={{ textDecoration: 'none' }}>
+/*            <li className="">Home</li>
+          </Link>
+          <Link to='/Login' className="grid-item" style={{ textDecoration: 'none' }}>
+            <li className="">Login</li>
+          </Link>
+          <Link to='/Profile' className="grid-item"  style={{ textDecoration: 'none' }}>
+          <li className="">Profile</li>
+          </Link>
+          <Link to='/Signup' className="grid-item" style={{ textDecoration: 'none' }}>
+          <li className="">Sign up</li>
+          </Link>
+          <Link to='/Account' className="grid-item" style={{ textDecoration: 'none' }}>
+          <li className="">Account</li>
+          </Link>
+        </ul>
+      </nav>>
+      </nav>
+    )
+  }
+  
+  
+  
+  export default Nav;
+  */
